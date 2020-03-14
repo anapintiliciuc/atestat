188d5950b8c853f6f70976fe19e3b301cfa52910
mariaana12 <ampintiliciuc@gmail.com>
2020-03-14 17:54:44 +0200

    Primul commit

--- ProiectOOp/Form1.cs
+++ ProiectOOp/Form1.cs
@@ -16,5 +16,10 @@ namespace ProiectOOp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Primul commit");
+        }
     }
 }
