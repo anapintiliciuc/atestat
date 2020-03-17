df59fb624a04c3096fbeb89a547c2bbc2ea52275
mariaana12 <ampintiliciuc@gmail.com>
2020-03-17 12:07:49 +0200

    boop

--- ProiectOOp/Form1.cs
+++ ProiectOOp/Form1.cs
@@ -20,6 +20,7 @@ namespace ProiectOOp
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Primul commit");
+            MessageBox.Show("Primul commit");
         }
     }
 }
